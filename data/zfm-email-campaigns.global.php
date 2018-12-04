6f85cd2a46dcd62add1960f464d4fcca46de5a03
AlejoLespaul <alesitoman@gmail.com>
2018-12-03 21:40:26 -0300

    Update zfm-email-campaigns.global.php

--- data/zfm-email-campaigns.global.php
+++ data/zfm-email-campaigns.global.php
@@ -4,6 +4,15 @@ return [
   'ZfMetal\EmailCampaigns.options' => [
     'url_domain'            => 'http://yourdomainhere/',
     'search_campaign_limit' => 5,
-    'path_attached_files'   => './files'
+    'path_attached_files'   => './files',
+    'distribution_record_fields' => [
+      '@nombre'   => 'firstName',
+      '@apellido' => 'lastName',
+      '@email'    => 'email',
+      '@telefono' => 'phone',
+      '@c1'       => 'customerField1',
+      '@c2'       => 'customerField2',
+      '@c3'       => 'customerField3',
+    ]
   ]
 ];
